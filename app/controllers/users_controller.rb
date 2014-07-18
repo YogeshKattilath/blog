83284365bda10ea758fbc65002f8570085000557
yogeshkattilath <yogeshkattilath1990@gmail.com>
2014-07-18 17:43:59 +0530

    Micropost done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   	
   end
 
@@ -56,9 +57,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
+    
 
     def correct_user
       @user = User.find(params[:id])
